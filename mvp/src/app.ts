e5044b005b5b9b00ee990f4cdf04c7bf791bdde4
Brett Sodie <blsodie@gmail.com>
2026-02-13 16:24:34 -0500

    feat: add cors

--- mvp/src/app.ts
+++ mvp/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { metricsIngestRouter } from './routes/ingest';
 import { metricsRouter } from './routes/metrics';
 import { tablesRouter } from './routes/tables';
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.raw({
   type: ['application/x-protobuf', 'application/octet-stream', 'application/json'],
