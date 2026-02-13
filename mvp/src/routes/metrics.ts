354a5d18d4d13340b5e9702548d10fd43e33865d
Brett Sodie <blsodie@gmail.com>
2026-02-13 16:31:32 -0500

    chore: cahnge log

--- mvp/src/routes/metrics.ts
+++ mvp/src/routes/metrics.ts
@@ -28,7 +28,7 @@ metricsRouter.get('/metrics', async (req, res) => {
   try {
         const data = await getRps({ serviceName, tenant, start, end, step });
         console.log('INNNN');
-        console.log(data.series.points);
+        console.log(data.series);
   } catch(e) {
     console.log(e);
   }
